db55878fa9bacbbc47f5dbe8b0fea2bc010fe4b3
Vinh Ha <vinh_ha_is@hotmail.com>
2023-11-30 14:52:20 +1100

    updated readme and main.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # azure-new
+Following ACG to create Azure resume
+
+## First steps
+
+- Frontend folder contains the website
+- main.js contains visitor counter code
---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DomContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const fucntionApi = '';
+
+const getVisitCount = () => {
+    let count = 30
+    fetch(fucntionApi).then(Response => {
+        return Response.json()
+    }).then(Response => {
+        console.log("Website called function API.");
+        count = Response.count;
+        document.getElementById("Counter").innerText = count;
+    }).catch(fucntion(error){
+        console.log(error);
+    });
+    return count;
+}
